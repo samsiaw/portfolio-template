14b558ac844342fd63158dcb623a44e9684e2216
Sam <samtsiaw@gmail.com>
2021-06-22 01:59:10 -0400

    renamed invalid paths

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('projects/', views.projects, name="projects"),
     path('projects/<int:project_id>/', views.single_project, name="single_project"),
     path('error/', views.error, name="error"),
-    re_path(r'error/.+', views.error, name="error"),
-    re_path(r'projects/.*\D+', views.error, name="error"),
+    re_path(r'error/.+', views.error, name="error_bad_path"),
+    re_path(r'projects/.*\D+', views.error, name="error_projects"),
 
 
     #path('about/', views.about, name="about")
